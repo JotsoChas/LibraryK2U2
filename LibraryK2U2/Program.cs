183d868e07a647c9bd86ece6b4d5cc3432043865
JocoChas <joco.borghol@chasacademy.se>
2025-12-13 18:49:00 +0100

    test to confirm the connection to database

--- LibraryK2U2/Program.cs
+++ LibraryK2U2/Program.cs
@@ -1,7 +1,6 @@
-﻿using LibraryK2U2.data;
-using LibraryK2U2.models;
-using System;
+﻿using System;
 using System.Linq;
+using LibraryK2U2.data;
 
 namespace LibraryK2U2
 {
@@ -11,17 +10,59 @@ namespace LibraryK2U2
         {
             using var db = new LibraryDBContext();
 
-            var books = db.Books.ToList();
-
-            Console.WriteLine("All books:");
+            // BOOKS
+            Console.WriteLine("BOOKS");
             Console.WriteLine("-----------------------------");
 
-            foreach (var b in books)
+            foreach (var b in db.Books.ToList())
             {
                 Console.WriteLine($"{b.BookId}: {b.Title} - {b.Author} - {b.Category}");
             }
 
+            // MEMBERS
+            Console.WriteLine();
+            Console.WriteLine("MEMBERS");
             Console.WriteLine("-----------------------------");
+
+            foreach (var m in db.Members.ToList())
+            {
+                Console.WriteLine($"{m.MemberId}: {m.FirstName} {m.LastName}");
+            }
+
+            // LOANS
+            Console.WriteLine();
+            Console.WriteLine("LOANS");
+            Console.WriteLine("-----------------------------");
+
+            foreach (var l in db.Loans.ToList())
+            {
+                Console.WriteLine(
+                    $"LoanId: {l.LoanId}, BookId: {l.BookId}, MemberId: {l.MemberId}, " +
+                    $"LoanDate: {l.LoanDate}, DueDate: {l.DueDate}, ReturnDate: {l.ReturnDate}"
+                );
+            }
+
+            // ACTIVE LOANS (VIEW)
+            Console.WriteLine();
+            Console.WriteLine("ACTIVE LOANS (VIEW)");
+            Console.WriteLine("-----------------------------");
+
+            foreach (var a in db.ActiveLoans.ToList())
+            {
+                Console.WriteLine($"{a.MemberName} -> {a.BookTitle}");
+            }
+
+            // RETURNED LOANS (VIEW)
+            Console.WriteLine();
+            Console.WriteLine("RETURNED LOANS (VIEW)");
+            Console.WriteLine("-----------------------------");
+
+            foreach (var r in db.ReturnedLoans.ToList())
+            {
+                Console.WriteLine($"{r.MemberName} -> {r.BookTitle}");
+            }
+
+            Console.WriteLine();
             Console.WriteLine("Done.");
             Console.ReadKey();
         }
